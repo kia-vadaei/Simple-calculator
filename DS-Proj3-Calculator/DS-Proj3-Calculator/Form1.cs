5c216f84a2c672e30d03ab4b29aa4edb1b1aeded
Kianoosh <kia.comp.eng@outlook.com>
2022-11-10 21:09:54 +0330

    UI is being complited

--- DS-Proj3-Calculator/DS-Proj3-Calculator/Form1.cs
+++ DS-Proj3-Calculator/DS-Proj3-Calculator/Form1.cs
@@ -13,18 +13,97 @@ namespace DS_Proj3_Calculator
 {
     public partial class Form1 : Form
     {
-
-    void set_butt_cur()
-    {
-      foreach (Control control in butt_pnl.Controls)
-      {
-        if (control is Button)
-          control.Cursor = Cursors.Hand;
-      }
-    }
+        public string MainPhrase = "";
         public Form1()
         {
             InitializeComponent();
         }
+
+
+
+
+
+
+
+
+
+
+        #region Button Settings
+        private void b1_Click(object sender, EventArgs e)
+        {
+            if (Lcd.Text == "0")
+                Lcd.Clear();
+            Lcd.Text += "1";
+        }
+
+        private void b2_Click(object sender, EventArgs e)
+        {
+            if (Lcd.Text == "0")
+                Lcd.Clear();
+            Lcd.Text += "2";
+        }
+
+        private void b3_Click(object sender, EventArgs e)
+        {
+            if (Lcd.Text == "0")
+                Lcd.Clear();
+            Lcd.Text += "3";
+        }
+
+        private void b4_Click(object sender, EventArgs e)
+        {
+            if (Lcd.Text == "0")
+                Lcd.Clear();
+            Lcd.Text += "4";
+        }
+
+        private void b5_Click(object sender, EventArgs e)
+        {
+            if (Lcd.Text == "0")
+                Lcd.Clear();
+            Lcd.Text += "5";
+        }
+
+        private void b6_Click(object sender, EventArgs e)
+        {
+            if (Lcd.Text == "0")
+                Lcd.Clear();
+            Lcd.Text += "6";
+        }
+
+        private void b7_Click(object sender, EventArgs e)
+        {
+            if (Lcd.Text == "0")
+                Lcd.Clear();
+            Lcd.Text += "7";
+        }
+
+        private void b8_Click(object sender, EventArgs e)
+        {
+            if (Lcd.Text == "0")
+                Lcd.Clear();
+            Lcd.Text += "8";
+        }
+
+        private void b9_Click(object sender, EventArgs e)
+        {
+            if (Lcd.Text == "0")
+                Lcd.Clear();
+            Lcd.Text += "9";
+        }
+
+        private void dot_butt_Click(object sender, EventArgs e)
+        {
+            if (!Lcd.Text.Contains("."))
+                Lcd.Text += ".";
+        }
+
+        private void clear_butt_Click(object sender, EventArgs e)
+        {
+            Lcd.Clear();
+            Lcd.Text = "0";
+        }
+        #endregion
+
     }
 }
